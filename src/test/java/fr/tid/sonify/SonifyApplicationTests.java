0693e628f475d53c9dca8aaba738451f5694cad5
cheikh tidiane <cheikh951812@gmail.com>
2024-09-11 18:55:17 +0200

    configuration

---
+++ src/test/java/fr/tid/sonify/SonifyApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.tid.sonify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
