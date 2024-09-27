22a2318d0c3e498d1470537485f7ba934797102c
cheikh tidiane <cheikh951812@gmail.com>
2024-09-27 22:36:10 +0200

    feat: songMapper

--- src/main/java/fr/tid/sonify/mapper/SongMapper.java
+++ src/main/java/fr/tid/sonify/mapper/SongMapper.java
@@ -1,6 +1,35 @@
 package fr.tid.sonify.mapper;
+import fr.tid.sonify.dto.ReadSongInfoDTO;
+import fr.tid.sonify.dto.SaveSongDto;
+import fr.tid.sonify.dto.SongAuthorVO;
+import fr.tid.sonify.dto.SongTitleVO;
+import fr.tid.sonify.model.Song;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface SongMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "publicId", ignore = true)
+    Song saveSongDTOToSong(SaveSongDto saveSongDTO);
+
+    @Mapping(target = "favorite", ignore = true)
+    ReadSongInfoDTO songToReadSongInfoDTO(Song song);
+
+    default SongTitleVO stringToSongTitleVO(String title){
+        return new SongTitleVO(title);
+    }
+
+    default SongAuthorVO stringToSongAuthorVO(String author){
+        return new SongAuthorVO(author);
+    }
+
+    default String songTitleVOToString(SongTitleVO title) {
+        return title.value();
+    }
+
+    default String songAuthorVOToString(SongAuthorVO author) {
+        return author.value();
+    }
 }
